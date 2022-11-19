b4c89e497cfd90a0be554d57dba614c95692c3e6
LightEach <lighteach@gmail.com>
2022-11-20 03:55:35 +0900

    왜 다 커밋이 안된거지?

--- EllyInfo/ContentPages/SkillManage.xaml.cs
+++ EllyInfo/ContentPages/SkillManage.xaml.cs
@@ -1,3 +1,8 @@
+using System.Reflection;
+using Microsoft.Maui;
+using EllyInfo.Models;
+using Newtonsoft.Json;
+
 namespace EllyInfo.ContentPages;
 
 public partial class SkillManage : ContentPage
@@ -5,7 +10,45 @@ public partial class SkillManage : ContentPage
 	public SkillManage()
 	{
 		InitializeComponent();
+
+        BindCareers();
 	}
+
+    private async void BindCareers()
+    {
+        await Task.Run(async () =>
+        {
+
+            using (Stream strmJson = await FileSystem.Current.OpenAppPackageFileAsync("SkillsInformation.json"))
+            {
+                if (strmJson != null)
+                {
+                    using (StreamReader sr = new StreamReader(strmJson))
+                    {
+                        string jsonContent = sr.ReadToEnd();
+                        SkillsInformation skillInfo = skillInfo = JsonConvert.DeserializeObject<SkillsInformation>(jsonContent);
+
+                        TableView tmp = this.tvCareers;
+
+                        TableSection tbSection = tvCareers.Root.First();
+                        // [2022-11-20] 이 부분에서 오류나서 경력 히스토리를 추가시키지 못함 니기미..
+                        //foreach (CareerList cl in skillInfo.CareerList)
+                        //{
+                        //    tbSection.Add(new TextCell
+                        //    {
+                        //        Text = cl.CorpName
+                        //        , Detail = cl.Works
+                        //    });
+                        //}
+                    }
+                }
+            }
+
+
+
+        });
+
+    }
 
     private async void btnAlertTest_Clicked(object sender, EventArgs e)
     {
---
+++ EllyInfo/Models/SkillsInformation.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace EllyInfo.Models
+{
+    public partial class SkillsInformation
+    {
+        [JsonProperty("currentPay")]
+        public string CurrentPay { get; set; }
+
+        [JsonProperty("hopedPay")]
+        public string HopedPay { get; set; }
+
+        [JsonProperty("privateInfo")]
+        public PrivateInfo PrivateInfo { get; set; }
+
+        [JsonProperty("educationInfoList")]
+        public List<EducationInfoList> EducationInfoList { get; set; }
+
+        [JsonProperty("lincenseInfoList")]
+        public List<LincenseInfoList> LincenseInfoList { get; set; }
+
+        [JsonProperty("careerList")]
+        public List<CareerList> CareerList { get; set; }
+
+        [JsonProperty("skills")]
+        public List<Skill> Skills { get; set; }
+    }
+
+    public partial class CareerList
+    {
+        [JsonProperty("corpName")]
+        public string CorpName { get; set; }
+
+        [JsonProperty("startYm")]
+        public string StartYm { get; set; }
+
+        [JsonProperty("endYm")]
+        public string EndYm { get; set; }
+
+        [JsonProperty("position")]
+        public string Position { get; set; }
+
+        [JsonProperty("works")]
+        public string Works { get; set; }
+    }
+
+    public partial class EducationInfoList
+    {
+        [JsonProperty("schoolName")]
+        public string SchoolName { get; set; }
+
+        [JsonProperty("schoolType")]
+        public string SchoolType { get; set; }
+
+        [JsonProperty("majorType")]
+        public string MajorType { get; set; }
+
+        [JsonProperty("graduationYm")]
+        public string GraduationYm { get; set; }
+    }
+
+    public partial class LincenseInfoList
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("acquisitionYmd")]
+        public string AcquisitionYmd { get; set; }
+
+        [JsonProperty("issuingAgency")]
+        public string IssuingAgency { get; set; }
+    }
+
+    public partial class PrivateInfo
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("birth")]
+        public string Birth { get; set; }
+
+        [JsonProperty("gender")]
+        public string Gender { get; set; }
+
+        [JsonProperty("address")]
+        public string Address { get; set; }
+
+        [JsonProperty("cellphone")]
+        public string Cellphone { get; set; }
+
+        [JsonProperty("millitaryServiceInfo")]
+        public string MillitaryServiceInfo { get; set; }
+
+        [JsonProperty("marrigeStatus")]
+        public bool MarrigeStatus { get; set; }
+    }
+
+    public partial class Skill
+    {
+        [JsonProperty("skillName")]
+        public string SkillName { get; set; }
+
+        [JsonProperty("level")]
+        public string Level { get; set; }
+    }
+}
+
